714a00159f5205deabd148ff50a5a562537bb056
Oteri Eyenike <teyenike1@gmail.com>
2022-04-11 14:43:53 +0100

    Fix: the variables not defined

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,25 +7,26 @@ import { orderBy, range } from 'lodash';
 const Gallery = () => {
   const [images, setImages] = useState(galleryData);
 
-  const renderImages = orderBy(images, 'position').map(
-    ({ img, title, subtitle, id, position }) => (
-      <Draggable draggableId={id.toString()} index={position} key={id}>
-        {(provided) => (
-          <div
-            className='card'
-            {...provided.draggableProps}
-            ref={provided.innerRef}
-            {...provided.dragHandleProps}>
-            <div className='card__info'>
-              <p className='card__info-title'>{title}</p>
-              <p className='card__info-subtitle'>{subtitle}</p>
-            </div>
-            <img src={img} alt={subtitle} />
+  const renderImages = orderBy(images, 'position').map((item) => (
+    <Draggable
+      draggableId={item.id.toString()}
+      index={item.position}
+      key={item.id}>
+      {(provided) => (
+        <div
+          className='card'
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}>
+          <div className='card__info'>
+            <p className='card__info-title'>{item.title}</p>
+            <p className='card__info-subtitle'>{item.subtitle}</p>
           </div>
-        )}
-      </Draggable>
-    )
-  );
+          <img src={item.img} alt={item.subtitle} />
+        </div>
+      )}
+    </Draggable>
+  ));
 
   const onDragEnd = (result) => {
     const { destination, source } = result;
@@ -41,53 +42,35 @@ const Gallery = () => {
     }
 
     const directionOfDrag =
-      destination.index > source.index ? 'greater' : 'less';
+      destination.index > source.index ? 'GREATER' : 'LESS';
 
     let affectedRange;
-    if (directionOfDrag === 'greater') {
+    if (directionOfDrag === 'GREATER') {
       affectedRange = range(source.index, destination.index + 1);
-    } else if (directionOfDrag === 'less') {
+    } else if (directionOfDrag === 'LESS') {
       affectedRange = range(destination.index, source.index);
     }
 
     const reOrderedImage = images.map((image) => {
       if (image.id === parseInt(result.draggableId)) {
         image.position = result.destination.index;
+
         return image;
       } else if (affectedRange.includes(image.position)) {
-        if (directionOfDrag === 'greater') {
+        if (directionOfDrag === 'GREATER') {
           image.position = image.position - 1;
+
           return image;
-        } else if (directionOfDrag === 'less') {
+        } else if (directionOfDrag === 'LESS') {
           image.position = image.position + 1;
-          return image;
-        } else {
+
           return image;
         }
+      } else {
+        return image;
       }
     });
     setImages(reOrderedImage);
-    // const reOrderedPlaylist = playlist.map((song) => {
-    //   if (song.id === parseInt(result.draggableId)) {
-    //     song.position = result.destination.index;
-    //     // console.log("CONDITION 1", song);
-    //     return song;
-    //   } else if (affectedRange.includes(song.position)) {
-    //     if (directionOfDrag === "GREATER") {
-    //       song.position = song.position - 1;
-    //       // console.log("CONDITION 2.1", song);
-    //       return song;
-    //     } else if (directionOfDrag === "LESS") {
-    //       song.position = song.position + 1;
-    //       // console.log("CONDITION 2.2", song);
-    //       return song;
-    //     }
-    //   } else {
-    //     // console.log("CONDITION 3", song);
-    //     return song;
-    //   }
-    // });
-    // setImages(reOrderedPlaylist);
   };
   return (
     <div>
