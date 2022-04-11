53c90420bd297cf1d7b7b2bd7b1e79fba5f0af5a
Oteri Eyenike <teyenike1@gmail.com>
2022-04-11 10:19:33 +0100

    Update: src image in data.js

--- src/data.js
+++ src/data.js
@@ -1,74 +1,74 @@
 export default [
   {
     id: 1,
-    title: "Travel",
-    subtitle: "Lost in the city is where I found myself",
+    title: 'Travel',
+    subtitle: 'Lost in the city is where I found myself',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443651/react%20dnd/DTS_The_Green_Copson_Londan_5077.jpg",
-    position: 1
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443651/react%20dnd/DTS_The_Green_Copson_Londan_5077.jpg',
+    position: 1,
   },
   {
     id: 2,
-    title: "Guide",
-    subtitle: "Guide to Iceland",
+    title: 'Guide',
+    subtitle: 'Guide to Iceland',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443650/react%20dnd/DTS_Closer_Parole_Dure_5144.jpg",
-    position: 2
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443650/react%20dnd/DTS_Closer_Parole_Dure_5144.jpg',
+    position: 2,
   },
   {
     id: 3,
-    title: "Journal",
-    subtitle: "A lost city on the rolling hills",
+    title: 'Journal',
+    subtitle: 'A lost city on the rolling hills',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443650/react%20dnd/DTS_Misc_1__Nich_Fancher__Nick_Fancher_4532.jpg",
-    position: 3
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443650/react%20dnd/DTS_Misc_1__Nich_Fancher__Nick_Fancher_4532.jpg',
+    position: 3,
   },
   {
     id: 4,
-    title: "Guide",
-    subtitle: "Top 10 things to consider when travelling",
+    title: 'Guide',
+    subtitle: 'Top 10 things to consider when travelling',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443800/react%20dnd/_MG_9747_nk0tz2_c_scale_w_509.jpg",
-    position: 4
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443800/react%20dnd/_MG_9747_nk0tz2_c_scale_w_509.jpg',
+    position: 4,
   },
   {
     id: 5,
-    title: "CSS Grid",
-    subtitle: "Style your web componenets faster",
+    title: 'CSS Grid',
+    subtitle: 'Style your web componenets faster',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443853/react%20dnd/teri4.jpg",
-    position: 5
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443651/react%20dnd/DTS_Pride_AgustA%CC%83_n_Farias_5543.jpg',
+    position: 5,
   },
   {
     id: 6,
-    title: "Guide",
-    subtitle: "Hands on review with the best gimbal",
+    title: 'Guide',
+    subtitle: 'Hands on review with the best gimbal',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443879/react%20dnd/teri12.jpg",
-    position: 6
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1642955238/image-placeholders/teri.jpg',
+    position: 6,
   },
   {
     id: 7,
-    title: "React",
-    subtitle: "Be innovative",
+    title: 'React',
+    subtitle: 'Be innovative',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443800/react%20dnd/teri_mgjkie_c_scale_w_527.jpg",
-    position: 7
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649443800/react%20dnd/teri_mgjkie_c_scale_w_527.jpg',
+    position: 7,
   },
   {
     id: 8,
-    title: "Innovate",
-    subtitle: "Create awesome experience",
+    title: 'Innovate',
+    subtitle: 'Create awesome experience',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649432567/react%20dnd/pexels-vitalina-9197509.jpg",
-    position: 8
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649432567/react%20dnd/pexels-vitalina-9197509.jpg',
+    position: 8,
   },
   {
     id: 9,
-    title: "Network",
-    subtitle: "Transition to a better and comfy style",
+    title: 'Network',
+    subtitle: 'Transition to a better and comfy style',
     img:
-      "https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649432565/react%20dnd/pexels-ali-pazani-3280132.jpg",
-    position: 9
-  }
+      'https://res.cloudinary.com/terieyenike/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1649432565/react%20dnd/pexels-ali-pazani-3280132.jpg',
+    position: 9,
+  },
 ];
